44696f46419c42e08d2284dba8b061190466ca1b
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-10-16 21:55:56 +0300

    update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://652c3d82d0d1df5273ef4a1a.mockapi.io/users";
+axios.defaults.baseURL = "https://652c3d82d0d1df5273ef4a1a.mockapi.io";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -11,7 +11,7 @@ export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("/user/signup", credentials);
+      const res = await axios.post("/users", credentials);
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
@@ -24,7 +24,7 @@ export const logIn = createAsyncThunk(
   "auth/login",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("/user/login", credentials);
+      const res = await axios.post("/users", credentials);
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
