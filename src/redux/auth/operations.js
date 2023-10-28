d46d2c2d5678f968ed6801393ba374f4c388c692
Yuliia Karas <117581403+Yuliia-Karas@users.noreply.github.com>
2023-10-28 18:39:58 +0300

    update operations.js

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
-axios.defaults.baseURL = "https://652c3d82d0d1df5273ef4a1a.mockapi.io";
+// axios.defaults.baseURL = "https://652c3d82d0d1df5273ef4a1a.mockapi.io";
+axios.defaults.baseURL = "https://backend-zb-yk.onrender.com";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
@@ -11,7 +12,8 @@ export const register = createAsyncThunk(
   "auth/register",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("/users", credentials);
+      const res = await axios.post("/api/auth/register", credentials);
+
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
@@ -24,7 +26,19 @@ export const logIn = createAsyncThunk(
   "auth/login",
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post("/users", credentials);
+      const res = await axios.post("/api/auth/login", credentials);
+      // const res = await fetch(
+      //   "https://backend-zb-yk.onrender.com/api/auth/login",
+      //   {
+      //     method: "POST",
+      //     headers: { "Content-Type": "application/json" },
+      //     body: JSON.stringify(credentials),
+      //   }
+      // );
+      // const json = await res.json();
+
+      // setAuthHeader(json.token);
+
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
